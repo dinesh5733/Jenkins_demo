8cfdbb39d3758fbd725551868333b8ff8429b369
dinesh5733 <ingoledinu01@gmail.com>
2025-09-15 11:44:13 +0530

    Updated Main.java with new changes

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -7,6 +7,6 @@ public class Main {
         System.out.println("This is Java Programme");
         System.out.println(String.valueOf(new Date()));
         System.out.println("first code added successfully..!");
-
+        System.out.println("Added modification..");
     }
 }
