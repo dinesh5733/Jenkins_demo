6b61e820c85e81639dddca4f42b0bbd64cddc2b5
dinesh5733 <ingoledinu01@gmail.com>
2025-09-15 11:00:49 +0530

    Initial Project Commit

---
+++ untitled/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+        System.out.printf("This is Java Programme");
+        System.out.printf(String.valueOf(new Date()));
+    }
+}
