a5778b768b304ee30d645f5df1539a401ce525bb
dinesh5733 <ingoledinu01@gmail.com>
2025-09-15 11:10:44 +0530

    code modified print stratergy

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -3,10 +3,10 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        System.out.printf("This is Java Programme");
-        System.out.printf(String.valueOf(new Date()));
-        System.out.printf("first code added successfully..!");
+        System.out.println("Hello and welcome!");
+        System.out.println("This is Java Programme");
+        System.out.println(String.valueOf(new Date()));
+        System.out.println("first code added successfully..!");
 
     }
 }
